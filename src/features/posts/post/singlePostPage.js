0f5188fbc5e7d7f1dc3b98f4249b4545fad3587f
yannw83 <yannickdjoa2@gmail.com>
2023-02-01 12:21:28 +0100

    useparams added to postlist

--- src/features/posts/post/singlePostPage.js
+++ src/features/posts/post/singlePostPage.js
@@ -1,13 +1,11 @@
 import React from 'react'
-import { useSelector } from 'react-redux';
-import { selectPostById } from '../postsSlice';
 
 import { useParams } from 'react-router-dom'
 
-const SinglePostPage = () => {
+const SinglePostPage = ({posts}) => {
     const {postId}= useParams();
-    const post= useSelector((state)=>selectPostById(state, Number(postId)));
-    if (!post) {
+    
+    if (!posts) {
         return (
             <section>
                 <h2>post not found!</h2>
@@ -16,11 +14,18 @@ const SinglePostPage = () => {
         
     }
   return (
-    <article>
-      <h3>{post.data.title}</h3>
-      <p>{post.data.selftext}</p>
-    </article>
-  )
-}
+    <section>
+      <div>
+        {posts.filter((post)=>post.data.title=== postId).map((post, index)=>(
+          <article key={index}>
+            <h3>{post.data.title}</h3>
+            <p>{post.data.selftext}</p>
+          </article>))}
+      </div>
+      
+    </section>
+    
+  );
+};
 
 export default SinglePostPage
